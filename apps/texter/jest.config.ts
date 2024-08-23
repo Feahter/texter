29f7450e5d25569e476d2e86d199b6424aa7f737
Arthur <arthur@lwork.com>
2024-08-23 22:34:35 +0800

    fix: 🐛 jest esm fixed
    jest esm fixed

--- apps/texter/jest.config.ts
+++ apps/texter/jest.config.ts
@@ -1,7 +1,15 @@
+/*
+ * @Author: Arthur arthur@lwork.com
+ * @Date: 2024-08-22 21:47:11
+ * @LastEditors: Arthur arthur@lwork.com
+ * @LastEditTime: 2024-08-23 22:33:33
+ * @FilePath: /texter/apps/texter/jest.config.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 /* eslint-disable */
 export default {
   displayName: 'texter',
-  preset: '../../jest.preset.js',
+  preset: '../../jest.preset.cjs',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
